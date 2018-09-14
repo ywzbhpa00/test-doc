338455a9d148ccb5280f05a6b13e3a06c07868d8
Andy <ywzbhpa00@126.com>
2018-09-14 17:05:10 +0800

    dd (#5)
    dddd

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const ride () = (a, b) => {
+  return a * b
+}
+
+const reduce = (a, b) => {
+  return a - b - 1
+}
+
 const add = (a, b, c) => {
   return a + b + c + 10
 }
 
 const ff = () => {
   return 'ffff'
+}
+
+const dd = () => {
+  return 'dd'
 }
 
 module.exports = pixivLogin
